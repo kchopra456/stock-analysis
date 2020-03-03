254ac867ea2f7083c036492b97e947ef80dcdade
Karan Chopra <kchopra456@gmail.com>
2020-03-04 05:44:05 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 docker run --rm  \
 -v "$(pwd)/.output":"/src/.output" \
 stock-analysis:latest \
- /bin/bash -c 'python3 app.py  --sql "SELECT * FROM MSFT, GOOGL WHERE Date >= \"2020-01-02\";" --tickers "MSFT" "GOOGL" -v'
+ /bin/bash -c 'python3 app.py  --sql "SELECT * FROM MSFT, GOOGL WHERE Date >= \"2020-01-02\";" --tickers "MSFT" "GOOGL" -v -o ./.output \
+--data Population ./assets/us-population.csv year SP.POP.TOTL'
